ff41c8bfddcf1e57a26b073994b57de5bcb3f41d
Pientaszek94 <89315537+Pientaszek94@users.noreply.github.com>
2022-08-12 22:04:29 +0200

    next upgrade

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
 
   const {isLoaded}=useJsApiLoader({googleMapsApiKey:"AIzaSyDZuApf86u95n8egqd4-cME5pO0zP6IIrk" , 
   libraries:['places'], });
-  if(!isLoaded) return <div><h2>Loading maps...</h2></div>
+  if(!isLoaded) return <div className='loading'><h2>Loading maps...</h2></div>
   return(
             <Layout>
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
